eafe9516b1e9dd0eb8f5d9a1e772bf3a832e0415
a1rsh1p <jwhenry28@gmail.com>
2024-10-09 07:57:40 -0400

    multi-line scrolling bug

--- terminal.gd
+++ terminal.gd
@@ -1,73 +1,79 @@
 class_name Terminal extends Panel
 
-const CHAR_WIDTH: int = 8
-const CHAR_HEIGHT: int = 16
-const CURSOR: String = "█"
-
-var font: Font
-var _font_size: int
-var _color: Color
+@export var _font: Font
+@export var _font_size: int
+@export var _font_color: Color
 @export var _text_border_size_x: int
 @export var _text_border_size_y: int
 
-var _buffer: Array
-var caps_lock_enabled: bool
+var _CHAR_WIDTH: float
+var _CHAR_HEIGHT: float
+const _CURSOR: String = "█"
 
-var cursor_right_limit: int
-var cursor_idx: int
+var _buffer: Array
+var _caps_lock_enabled: bool
+
+var _cursor_right_limit: int
+var _cursor_idx: int
 var _start_line_idx: int
 
 var _cmd_string
-var _cmd_strings: Array
+var _terminal_history: Array
 var _last_cmd_idx: int
 
 
 func get_num_window_rows() -> int:
-	return floor((size.y - (_text_border_size_y * 2)) / CHAR_HEIGHT) - 1
+	return floor(get_window_y() / _CHAR_HEIGHT) - 1
+
+
+func get_window_x() -> int:
+	return size.x - (_text_border_size_x * 2) - _CHAR_WIDTH
+
+
+func get_window_y() -> int:
+	return size.y - (_text_border_size_y * 2) - _CHAR_HEIGHT
 
 
 func get_num_rows_in_buffer() -> int:
 	var num_rows_in_buffer = 0
 	var buffer_string = "".join(PackedStringArray(_buffer.slice(0, _buffer.size()-1)))
 	for item in buffer_string.split("\n"):
-		var tmp_rows = ceil((item.length() * CHAR_WIDTH) / size.x)
-		print("get_num_rows_in_buffer: tmp_rows=", tmp_rows)
+		var tmp_rows = ceil((item.length() * _CHAR_WIDTH) / get_window_x())
+		if item.begins_with("Lorem"):
+			print("get_num_rows_in_buffer: num_chars=", item.length())
+			print("get_num_rows_in_buffer: CHAR_WIDTH=", _CHAR_WIDTH)
+			print("get_num_rows_in_buffer: size.x=", get_window_x())
+			print("get_num_rows_in_buffer: raw_calc=", (item.length() * _CHAR_WIDTH) / get_window_x())
+			print("get_num_rows_in_buffer: tmp_rows=", tmp_rows)
 		num_rows_in_buffer += tmp_rows
 	print("get_num_rows_in_buffer: ret=", num_rows_in_buffer)
 	return num_rows_in_buffer
 
 
-func _init(theme_name: String = "") -> void:
-	if theme_name != "":
-		theme = load(theme_name)
-	font = preload("res://fonts/SFMonoMedium.otf")# TODO - move this into constructor
+func _init() -> void:
 	_buffer = [ null ]
-	caps_lock_enabled = false
-	cursor_idx = 0
-	cursor_right_limit = 0
-	_cmd_strings = [ "" ]
-	_last_cmd_idx = 0
 	_cmd_string = null
-	_start_line_idx = 0
+	_terminal_history = [ "" ]
 	
-	_font_size = 16 # TODO - move this into constructor
-	_color = Color8(57, 255, 20, 255) # TODO - move this into constructor
+	print("init: font_size=", _font_size)
+	
+	_CHAR_HEIGHT = 16.0 # TODO: MOVE THIS
+	_CHAR_WIDTH = 8.0 # TODO: MOVE THIS
+	_caps_lock_enabled = false
+	
+	_cursor_idx = 0
+	_cursor_right_limit = 0
+	_last_cmd_idx = 0
+	_start_line_idx = 0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	grab_focus()
 	print(size)
 	
-	#for i in range(64):
-		#write(str(i) + "\n")
-	write("WELCOME TO TELCO1\nTYPE 'HELP' TO BEGIN:\n")
-	write("> ")
-	
-	for i in range(32):
-		var cmds = ["whoami", "pwd", "test"]
-		var cmd = cmds[i % cmds.size()]
-		write(cmd + "\n")
-		run_command(cmd)
+	write("> lorem\n")
+	run_command("lorem")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -76,22 +82,20 @@ func _process(_delta: float) -> void:
 
 
 func _draw():
-	var x_limit = size.x - CHAR_WIDTH
-	var y_limit = size.y - CHAR_HEIGHT
+	var x_limit = get_window_x() + _text_border_size_x
+	var y_limit = get_window_y() + _text_border_size_y
 	
 	var x_start = _text_border_size_x
-	var y_start = CHAR_HEIGHT + _text_border_size_y
+	var y_start = _CHAR_HEIGHT + _text_border_size_y
 	var char_pos = Vector2(x_start, y_start)
 	
 	var idx = 0
-	
-	print("write: (draw) _num_rows_in_buffer=", get_num_rows_in_buffer())
-	
+		
 	var line_idx = 0
 	var start_line_index = _start_line_idx
 	var end_line_index = _start_line_idx + min(get_num_window_rows(), get_num_rows_in_buffer())
 	
-	print("draw: cursor_idx=", cursor_idx)
+	print("draw: _cursor_idx=", _cursor_idx)
 	print("draw: start_line_index=", start_line_index)
 	print("draw: end_line_index=", end_line_index)
 	print("draw: num_window_rows=", get_num_window_rows())
@@ -106,18 +110,18 @@ func _draw():
 				draw_key = " "
 				
 			if idx_in_range:
-				draw_char(font, char_pos, draw_key, _font_size, _color)
-				char_pos.x += CHAR_WIDTH
+				draw_char(_font, char_pos, draw_key, _font_size, _font_color)
+				char_pos.x += _CHAR_WIDTH
 				
 			if key == "\n" or char_pos.x >= x_limit:
 				if idx_in_range:
 					char_pos.x = _text_border_size_x
-					char_pos.y += CHAR_HEIGHT
+					char_pos.y += _CHAR_HEIGHT
 				line_idx += 1
 			
-		if idx_in_range and idx == cursor_idx:
+		if idx_in_range and idx == _cursor_idx:
 			print("draw: drawing cursor on [", key, "]")
-			draw_char(font, char_pos, CURSOR, _font_size, _color)
+			draw_char(_font, char_pos, _CURSOR, _font_size, _font_color)
 		
 		idx += 1
 
@@ -132,17 +136,19 @@ func _gui_input(event: InputEvent) -> void:
 					_start_line_idx = min(_start_line_idx + 1, get_num_rows_in_buffer() - get_num_window_rows())
 				else:
 					_start_line_idx = 0
+				print("event: _start_line_idx=", _start_line_idx)
 			if event.button_index == MOUSE_BUTTON_WHEEL_UP:
 				print("event: wheel down")
 				print(event)
 				_start_line_idx = max(0, _start_line_idx - 1)
+				print("event: _start_line_idx=", _start_line_idx)
 			queue_redraw()
 	
 	if event is InputEventKey and event.pressed and !event.is_echo():
 		var c = event_to_char(event)
 		if c != "":
 			add_to_buffer(c, c=="\n")
-			cursor_idx = _buffer.size() - 1
+			_cursor_idx = _buffer.size() - 1
 		queue_redraw()
 		
 		if _cmd_string != null:
@@ -153,33 +159,33 @@ func _gui_input(event: InputEvent) -> void:
 
 
 func clear_current_line() -> void:
-	while cursor_idx > cursor_right_limit:
-		_buffer.remove_at(cursor_right_limit)
-		cursor_idx -= 1
+	while _cursor_idx > _cursor_right_limit:
+		_buffer.remove_at(_cursor_right_limit)
+		_cursor_idx -= 1
 		
 
 
 func add_to_buffer(text: String, append_to_buffer: bool = false) -> void:
 	assert(text.length() == 1, "add_to_buffer: should only add char (input text len=" + str(text.length()) + ")")
 	
-	#print("add: cursor_idx=", cursor_idx, " _buffer.size=", _buffer.size())
+	#print("add: _cursor_idx=", _cursor_idx, " _buffer.size=", _buffer.size())
 	if append_to_buffer:
-		print("add: cursor_idx=", cursor_idx, " _buffer.size=", _buffer.size())
+		print("add: _cursor_idx=", _cursor_idx, " _buffer.size=", _buffer.size())
 		print("add: replacing character: ", _buffer[_buffer.size() - 1], " with ", text)
 		_buffer.insert(_buffer.size() - 1, text)
 	else:
-		_buffer.insert(cursor_idx, text)
+		_buffer.insert(_cursor_idx, text)
 	
 	if _buffer[-1] != null:
 		_buffer.append(null)
-	cursor_idx += 1
+	_cursor_idx += 1
 
 
 func write(text: String) -> void:
-	var char_row_count = int(size.x / CHAR_WIDTH)
+	var char_row_count = int(size.x / _CHAR_WIDTH)
 	for c in text:
 		add_to_buffer(c)
-	cursor_right_limit = cursor_idx
+	_cursor_right_limit = _cursor_idx
 	print("write: max=", get_num_rows_in_buffer() - get_num_window_rows())
 	_start_line_idx = max(0, get_num_rows_in_buffer() - get_num_window_rows())
 	print("write: _start_line_idx=", _start_line_idx)
@@ -201,7 +207,7 @@ func run_command(cmd_string: String) -> void:
 
 
 func get_cmd_string() -> String:
-	return "".join(PackedStringArray(_buffer.slice(cursor_right_limit, _buffer.size()-1)))
+	return "".join(PackedStringArray(_buffer.slice(_cursor_right_limit, _buffer.size()-1)))
 
 
 func event_to_char(event: InputEventKey) -> String:
@@ -212,157 +218,157 @@ func event_to_char(event: InputEventKey) -> String:
 	print("event: ", keycode_string)
 	
 	match keycode_string:
-		"A" when caps_lock_enabled:
+		"A" when _caps_lock_enabled:
 			c = "A"
 		"A":
 			c = "a"
 		"Shift+A":
 			c = "A"
-		"B" when caps_lock_enabled:
+		"B" when _caps_lock_enabled:
 			c = "B"
 		"B":
 			c = "b"
 		"Shift+B":
 			c = "B"
-		"C" when caps_lock_enabled:
+		"C" when _caps_lock_enabled:
 			c = "C"
 		"C":
 			c = "c"
 		"Shift+C":
 			c = "C"
-		"D" when caps_lock_enabled:
+		"D" when _caps_lock_enabled:
 			c = "D"
 		"D":
 			c = "d"
 		"Shift+D":
 			c = "D"
-		"E" when caps_lock_enabled:
+		"E" when _caps_lock_enabled:
 			c = "E"
 		"E":
 			c = "e"
 		"Shift+E":
 			c = "E"
-		"F" when caps_lock_enabled:
+		"F" when _caps_lock_enabled:
 			c = "F"
 		"F":
 			c = "f"
 		"Shift+F":
 			c = "F"
-		"G" when caps_lock_enabled:
+		"G" when _caps_lock_enabled:
 			c = "G"
 		"G":
 			c = "g"
 		"Shift+G":
 			c = "G"
-		"H" when caps_lock_enabled:
+		"H" when _caps_lock_enabled:
 			c = "H"
 		"H":
 			c = "h"
 		"Shift+H":
 			c = "H"
-		"I" when caps_lock_enabled:
+		"I" when _caps_lock_enabled:
 			c = "I"
 		"I":
 			c = "i"
 		"Shift+I":
 			c = "I"
-		"J" when caps_lock_enabled:
+		"J" when _caps_lock_enabled:
 			c = "J"
 		"J":
 			c = "j"
 		"Shift+J":
 			c = "J"
-		"K" when caps_lock_enabled:
+		"K" when _caps_lock_enabled:
 			c = "K"
 		"K":
 			c = "k"
 		"Shift+K":
 			c = "K"
-		"L" when caps_lock_enabled:
+		"L" when _caps_lock_enabled:
 			c = "L"
 		"L":
 			c = "l"
 		"Shift+L":
 			c = "L"
-		"M" when caps_lock_enabled:
+		"M" when _caps_lock_enabled:
 			c = "M"
 		"M":
 			c = "m"
 		"Shift+M":
 			c = "M"
-		"N" when caps_lock_enabled:
+		"N" when _caps_lock_enabled:
 			c = "N"
 		"N":
 			c = "n"
 		"Shift+N":
 			c = "N"
-		"O" when caps_lock_enabled:
+		"O" when _caps_lock_enabled:
 			c = "O"
 		"O":
 			c = "o"
 		"Shift+O":
 			c = "O"
-		"P" when caps_lock_enabled:
+		"P" when _caps_lock_enabled:
 			c = "P"
 		"P":
 			c = "p"
 		"Shift+P":
 			c = "P"
-		"Q" when caps_lock_enabled:
+		"Q" when _caps_lock_enabled:
 			c = "Q"
 		"Q":
 			c = "q"
 		"Shift+Q":
 			c = "Q"
-		"R" when caps_lock_enabled:
+		"R" when _caps_lock_enabled:
 			c = "R"
 		"R":
 			c = "r"
 		"Shift+R":
 			c = "R"
-		"S" when caps_lock_enabled:
+		"S" when _caps_lock_enabled:
 			c = "S"
 		"S":
 			c = "s"
 		"Shift+S":
 			c = "S"
-		"T" when caps_lock_enabled:
+		"T" when _caps_lock_enabled:
 			c = "T"
 		"T":
 			c = "t"
 		"Shift+T":
 			c = "T"
-		"U" when caps_lock_enabled:
+		"U" when _caps_lock_enabled:
 			c = "U"
 		"U":
 			c = "u"
 		"Shift+U":
 			c = "U"
-		"V" when caps_lock_enabled:
+		"V" when _caps_lock_enabled:
 			c = "V"
 		"V":
 			c = "v"
 		"Shift+V":
 			c = "V"
-		"W" when caps_lock_enabled:
+		"W" when _caps_lock_enabled:
 			c = "W"
 		"W":
 			c = "w"
 		"Shift+W":
 			c = "W"
-		"X" when caps_lock_enabled:
+		"X" when _caps_lock_enabled:
 			c = "X"
 		"X":
 			c = "x"
 		"Shift+X":
 			c = "X"
-		"Y" when caps_lock_enabled:
+		"Y" when _caps_lock_enabled:
 			c = "Y"
 		"Y":
 			c = "y"
 		"Shift+Y":
 			c = "Y"
-		"Z" when caps_lock_enabled:
+		"Z" when _caps_lock_enabled:
 			c = "Z"
 		"Z":
 			c = "z"
@@ -455,46 +461,46 @@ func event_to_char(event: InputEventKey) -> String:
 		"Space":
 			c = " "
 		"CapsLock":
-			caps_lock_enabled = !caps_lock_enabled
+			_caps_lock_enabled = !_caps_lock_enabled
 		"Enter":
 			_cmd_string = get_cmd_string()
-			if _cmd_string != _cmd_strings[_cmd_strings.size() - 2]:
-				_cmd_strings[-1] = _cmd_string
-				_cmd_strings.append("")
-			_last_cmd_idx = _cmd_strings.size() - 1
+			if _cmd_string != _terminal_history[_terminal_history.size() - 2]:
+				_terminal_history[-1] = _cmd_string
+				_terminal_history.append("")
+			_last_cmd_idx = _terminal_history.size() - 1
 			print("event: cmd_string=", _cmd_string)
 			c = "\n"
 		"Left": 
-			print("event: cursor_right_limit=", cursor_right_limit, " cursor_idx=", cursor_idx)
-			cursor_idx = max(cursor_right_limit, cursor_idx - 1)
-			while cursor_idx > cursor_right_limit and _buffer[cursor_idx] == "\n":
-				cursor_idx = cursor_idx - 1
+			print("event: _cursor_right_limit=", _cursor_right_limit, " _cursor_idx=", _cursor_idx)
+			_cursor_idx = max(_cursor_right_limit, _cursor_idx - 1)
+			while _cursor_idx > _cursor_right_limit and _buffer[_cursor_idx] == "\n":
+				_cursor_idx = _cursor_idx - 1
 		"Right": 
-			cursor_idx = min(cursor_idx + 1, _buffer.size() - 1)
-			while cursor_idx < _buffer.size() and _buffer[cursor_idx] == "\n":
-				cursor_idx = cursor_idx + 1
+			_cursor_idx = min(_cursor_idx + 1, _buffer.size() - 1)
+			while _cursor_idx < _buffer.size() and _buffer[_cursor_idx] == "\n":
+				_cursor_idx = _cursor_idx + 1
 		"Up": 
 			print("event: _last_cmd_idx=", _last_cmd_idx)
-			if _cmd_strings.size() > 0 and _last_cmd_idx > -1:
+			if _terminal_history.size() > 0 and _last_cmd_idx > -1:
 				clear_current_line()
 				_last_cmd_idx = max(0, _last_cmd_idx - 1)
-				var last_cmd = _cmd_strings[_last_cmd_idx]
+				var last_cmd = _terminal_history[_last_cmd_idx]
 				print("event: last_cmd=", last_cmd)
 				for _c in last_cmd:
 					add_to_buffer(_c)
 		"Down": 
 			print("event: _last_cmd_idx=", _last_cmd_idx)
-			if _cmd_strings.size() > 0 and _last_cmd_idx < _cmd_strings.size()-1:
+			if _terminal_history.size() > 0 and _last_cmd_idx < _terminal_history.size()-1:
 				clear_current_line()
-				_last_cmd_idx = min(_last_cmd_idx + 1, _cmd_strings.size() - 1)
-				var last_cmd = _cmd_strings[_last_cmd_idx]
+				_last_cmd_idx = min(_last_cmd_idx + 1, _terminal_history.size() - 1)
+				var last_cmd = _terminal_history[_last_cmd_idx]
 				print("event: last_cmd=", last_cmd)
 				for _c in last_cmd:
 					add_to_buffer(_c)
 		"Backspace":
-			if cursor_idx > cursor_right_limit:
-				_buffer.remove_at(cursor_idx - 1)
-				cursor_idx -= 1
+			if _cursor_idx > _cursor_right_limit:
+				_buffer.remove_at(_cursor_idx - 1)
+				_cursor_idx -= 1
 		_:
 			c = ""
 	
